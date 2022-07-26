e0d79ada81ca2457fb31d8412ae4793388be4a6b
Avinash612 <87130586+Avinash612@users.noreply.github.com>
2022-07-26 18:24:29 +0530

    Update App.js
    react todo with local storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 import InputForm from "./components/TodoInput";
 import Tabs from "./components/Filtertabs";
 import Todo from "./components/Todolist";
-import { nanoid } from "nanoid";
 import "./App.css";
 /* eslint-disable */
+
+const AllTask = "All";
+const ActiveTask = "Active";
+const CompletedTask = "Completed";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filteredActiveItems, setFilteredActiveItems] = useState(AllTask);
+  {
+    useEffect(() => {
+      const listFrom = JSON.parse(localStorage.getItem("name"));
+      if (listFrom?.length > 0) {
+        setTasks(listFrom);
+      }
+    }, []);
+    console.log(tasks, "tasks");
+    useEffect(() => {
+      localStorage.setItem("name", JSON.stringify(tasks));
+    }, [tasks]);
+  }
 
-  useEffect(() => {
-    const listFrom = localStorage.getItem("name");
-    if (listFrom?.length > 0) {
-      setTasks(listFrom);
+  const getFilteredTasks = () => {
+    if (filteredActiveItems === AllTask) {
+      return tasks;
+    } else if (filteredActiveItems === CompletedTask) {
+      const updatedTasks = tasks.filter((task) => {
+        return task.completed === true;
+      });
+      return updatedTasks;
+    } else if (filteredActiveItems === ActiveTask) {
+      const activeTasks = tasks.filter((task) => {
+        return task.completed === false;
+      });
+      return activeTasks;
     }
-  }, []);
+    return tasks;
+  };
 
-  console.log(tasks, "tasks");
-  useEffect(() => {
-    window.localStorage.setItem("name", tasks);
-  }, [tasks]);
-
-  const taskList = tasks?.map((task) => (
+  const taskList = getFilteredTasks().map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -33,7 +55,8 @@ function App(props) {
   ));
   function addTask(name) {
     const newTask = { id: "todo" + nanoid(), name: name, completed: false };
-    setTasks([...tasks, newTask]);
+    const newTasks = [...tasks, newTask];
+    setTasks(newTasks);
   }
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
@@ -57,14 +80,33 @@ function App(props) {
     });
     setTasks(editedTaskList);
   }
+  //ALL TASKS......
+  const handleAllListClick = () => {
+    setFilteredActiveItems(AllTask);
+  };
+  // COMPLETED TASKS.....
+  const handleCompletedListClick = () => {
+    setFilteredActiveItems(CompletedTask);
+  };
+
+  //ACTIVE TASKS....
+
+  const handleActiveListClick = () => {
+    setFilteredActiveItems(ActiveTask);
+  };
+
   return (
     <div className="head-container">
       <style>{"body { background-color: black; }"}</style>
       <h1 className="heading">Todo</h1>
-      <InputForm options={taskList} handleAddButtonClick={addTask} />
+      <InputForm handleAddButtonClick={addTask} />
       <br />
       <div>
-        <Tabs />
+        <Tabs
+          onAllListClick={handleAllListClick}
+          onCompletedListClick={handleCompletedListClick}
+          onActiveListClick={handleActiveListClick}
+        />
       </div>
       <div>
         <ul role="list" className="unorderedlists">
